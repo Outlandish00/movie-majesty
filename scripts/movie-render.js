07ac74d8880a2813bf7e2871eb89475bf0efc1dd
Tyler Parker <tyler.parker13@hotmail.com>
2024-09-12 09:43:52 -0500

    Implemented main algorith in main module

--- scripts/movie-render.js
+++ scripts/movie-render.js
@@ -2,7 +2,8 @@ export const renderMoviesToDOM = (moviesHTML) => {
     const movieList = document.getElementById('movie-list');
 
     if (movieList){
-        movieList.innerHTML = movieHTML;}
+        movieList.innerHTML = moviesHTML;
+    }
     else {
         console.error('Could not find elements with id "movie-list"');
     }
